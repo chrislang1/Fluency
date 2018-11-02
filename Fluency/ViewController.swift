24ef117935c3c386a0e443359f0c3a2d0fb59a65
Chris Lang <c_langy@hotmail.com>
2018-11-02 13:21:45 +1030

    Initial Commit

---
+++ Fluency/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fluency
+//
+//  Created by Chris Lang on 2/11/18.
+//  Copyright © 2018 Christopher Lang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
