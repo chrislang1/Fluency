0dc10e1cfbe34845f22b4596e6855f893ef08406
Chris Lang <c_langy@hotmail.com>
2018-11-04 18:33:28 +1030

    Updated app icons. Added close icon. Added settings view.

---
+++ Fluency/SettingsViewController.swift
@@ -0,0 +1,34 @@
+//
+//  SettingsViewController.swift
+//  Fluency
+//
+//  Created by Chris Lang on 4/11/18.
+//  Copyright © 2018 Christopher Lang. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func supportButtonPressed(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func rateButtonPressed(_ sender: UIButton) {
+    }
+    
+
+}
