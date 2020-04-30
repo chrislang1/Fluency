395004fda92a1d2591f4465059baf57643857727
Christopher Lang <chrislang015@gmail.com>
2020-04-30 12:29:09 +0930

    FIX: Add in app email support. Add alternative email clients to check and open if user hasn't setup up default mail client.

--- Fluency/SettingsViewController.swift
+++ Fluency/SettingsViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import MessageUI
 
 class SettingsViewController: UIViewController {
 
@@ -23,15 +24,15 @@ class SettingsViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func closeSettings() {
+        if let delegate = delegate {
+            let delegateParentVC = delegate.delegate as! MainViewController
+            UIView.animate(withDuration: 0.3) {
+                delegateParentVC.view.alpha = 1
+            }
+        }
+        dismiss(animated: true, completion: nil)
     }
-    */
     
     @IBAction func hapticFeedbackSwitchChanged(_ sender: UISwitch) {
         hapticFeedbackSwitch.isOn = !hapticFeedbackSwitch.isOn
@@ -39,11 +40,7 @@ class SettingsViewController: UIViewController {
     }
     
     @IBAction func supportButtonPressed(_ sender: UIButton) {
-        //Adjust with new URL
-        if let url = NSURL(string: "http://christopherlang.com.au"){
-            UIApplication.shared.open(url as URL, options: [:], completionHandler: nil)
-        }
-        
+        sendEmail()
     }
     @IBAction func rateButtonPressed(_ sender: UIButton) {
         //Adjust for actual app id - links to expenses
@@ -53,12 +50,56 @@ class SettingsViewController: UIViewController {
     }
     
     @IBAction func exitButtonPressed(_ sender: UIButton) {
-        if let delegate = delegate {
-            let delegateParentVC = delegate.delegate as! MainViewController
-            UIView.animate(withDuration: 0.3) {
-                delegateParentVC.view.alpha = 1
+        closeSettings()
+    }
+                                              
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        closeSettings()
+    }
+}
+
+extension SettingsViewController: MFMailComposeViewControllerDelegate {
+    func sendEmail() {
+        if MFMailComposeViewController.canSendMail() {
+                let mail = MFMailComposeViewController()
+                mail.mailComposeDelegate = self
+                mail.setToRecipients(["chris@christopherlang.com.au"])
+
+                present(mail, animated: true)
+            } else if let emailUrl = createEmailUrl(to: "chris@christopherlang.com.au", subject: "", body: "") {
+                UIApplication.shared.open(emailUrl)
+        } else {
+            if let url = NSURL(string: "http://christopherlang.com.au"){
+                UIApplication.shared.open(url as URL, options: [:], completionHandler: nil)
             }
         }
-        dismiss(animated: true, completion: nil)
+    }
+
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        controller.dismiss(animated: true)
+    }
+    
+    private func createEmailUrl(to: String, subject: String, body: String) -> URL? {
+        let subjectEncoded = subject.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)!
+        let bodyEncoded = body.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)!
+
+        let gmailUrl = URL(string: "googlegmail://co?to=\(to)&subject=\(subjectEncoded)&body=\(bodyEncoded)")
+        let outlookUrl = URL(string: "ms-outlook://compose?to=\(to)&subject=\(subjectEncoded)")
+        let yahooMail = URL(string: "ymail://mail/compose?to=\(to)&subject=\(subjectEncoded)&body=\(bodyEncoded)")
+        let sparkUrl = URL(string: "readdle-spark://compose?recipient=\(to)&subject=\(subjectEncoded)&body=\(bodyEncoded)")
+        let defaultUrl = URL(string: "mailto:\(to)?subject=\(subjectEncoded)&body=\(bodyEncoded)")
+
+        if let gmailUrl = gmailUrl, UIApplication.shared.canOpenURL(gmailUrl) {
+            return gmailUrl
+        } else if let outlookUrl = outlookUrl, UIApplication.shared.canOpenURL(outlookUrl) {
+            return outlookUrl
+        } else if let yahooMail = yahooMail, UIApplication.shared.canOpenURL(yahooMail) {
+            return yahooMail
+        } else if let sparkUrl = sparkUrl, UIApplication.shared.canOpenURL(sparkUrl) {
+            return sparkUrl
+        }
+
+        return defaultUrl
     }
 }
